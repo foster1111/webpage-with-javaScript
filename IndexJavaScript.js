2ca07b0369aad7d3e8052f1c7c24599270d24adf
foster1111 <foster11112014@hotmail.com>
2022-09-27 03:57:37 +0100

    first upload

---
+++ IndexJavaScript.js
@@ -0,0 +1,142 @@
+"use strict";
+
+// cards javascript
+const card = document.querySelector(".card__card1");
+const card2 = document.querySelector(".card__card2");
+const card3 = document.querySelector(".card__card3");
+const card4 = document.querySelector(".card__card4");
+const card5 = document.querySelector(".card__card5");
+const card6 = document.querySelector(".card__card6");
+function clickRotate() {
+  card.classList.toggle("rotated");
+}
+function clickRotate2() {
+  card2.classList.toggle("rotated");
+}
+function clickRotate3() {
+  card3.classList.toggle("rotated");
+}
+function clickRotate4() {
+  card4.classList.toggle("rotated");
+}
+function clickRotate5() {
+  card5.classList.toggle("rotated");
+}
+function clickRotate6() {
+  card6.classList.toggle("rotated");
+}
+card.addEventListener("click", clickRotate);
+card2.addEventListener("click", clickRotate2);
+card3.addEventListener("click", clickRotate3);
+card4.addEventListener("click", clickRotate4);
+card5.addEventListener("click", clickRotate5);
+card6.addEventListener("click", clickRotate6);
+
+// testimonials
+
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(
+    document.getElementById("testim-dots").children
+  ),
+  testimContent = Array.prototype.slice.call(
+    document.getElementById("testim-content").children
+  ),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  // testimSpeed = 8500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+window.onload = function () {
+  // Testim Script
+  function playSlide(slide) {
+    for (var k = 0; k < testimDots.length; k++) {
+      testimContent[k].classList.remove("active");
+      testimContent[k].classList.remove("inactive");
+      testimDots[k].classList.remove("active");
+    }
+
+    if (slide < 0) {
+      slide = currentSlide = testimContent.length - 1;
+    }
+
+    if (slide > testimContent.length - 1) {
+      slide = currentSlide = 0;
+    }
+
+    if (currentActive != currentSlide) {
+      testimContent[currentActive].classList.add("inactive");
+    }
+    testimContent[slide].classList.add("active");
+    testimDots[slide].classList.add("active");
+
+    currentActive = currentSlide;
+
+    clearTimeout(testimTimer);
+    testimTimer = setTimeout(function () {
+      playSlide((currentSlide += 1));
+    }, testimSpeed);
+  }
+
+  testimLeftArrow.addEventListener("click", function () {
+    playSlide((currentSlide -= 1));
+  });
+
+  testimRightArrow.addEventListener("click", function () {
+    playSlide((currentSlide += 1));
+  });
+
+  for (var l = 0; l < testimDots.length; l++) {
+    testimDots[l].addEventListener("click", function () {
+      playSlide((currentSlide = testimDots.indexOf(this)));
+    });
+  }
+
+  playSlide(currentSlide);
+
+  // keyboard shortcuts
+  document.addEventListener("keyup", function (e) {
+    switch (e.keyCode) {
+      case 37:
+        testimLeftArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      case 39:
+        testimRightArrow.click();
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  testim.addEventListener("touchstart", function (e) {
+    touchStartPos = e.changedTouches[0].clientX;
+  });
+
+  testim.addEventListener("touchend", function (e) {
+    touchEndPos = e.changedTouches[0].clientX;
+
+    touchPosDiff = touchStartPos - touchEndPos;
+
+    console.log(touchPosDiff);
+    console.log(touchStartPos);
+    console.log(touchEndPos);
+
+    if (touchPosDiff > 0 + ignoreTouch) {
+      testimLeftArrow.click();
+    } else if (touchPosDiff < 0 - ignoreTouch) {
+      testimRightArrow.click();
+    } else {
+      return;
+    }
+  });
+};
